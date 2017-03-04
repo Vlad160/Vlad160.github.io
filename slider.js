764d7220867e755fe7f7ee750a1d04cf62e5805b
Vlad160 <vlaner160@gmail.com>
2017-03-05 02:14:33 +0300

    slider fixes and visual update

--- slider.js
+++ slider.js
@@ -9,7 +9,6 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -19,11 +18,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
 function slideTime(n) {
     n = 1
